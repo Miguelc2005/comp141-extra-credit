11758343e0e06d269149778a99b5e90353b3f305
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-12-18 03:18:15 +0000

    initial commit

--- main.sh
+++ main.sh
@@ -1,12 +1,21 @@
-This
-is 
-For
-A
-Extra
-Credit 
-Assignment
-To
-Help
-My
-Grade
-Thanks.
+#!/bin/sh
+mkdir output
+cp text.txt output/
+cd output
+cat text.txt > read.txt
+pwd > pwd.txt
+ls >ls.txt
+cp text.txt copy.txt
+alias printdate='date "+%Y-%m-%d"'
+printdate > date.txt
+wc text.txt > textcount.txt
+ps | head -n 5 > process.txt
+ifconfig | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+touch permissions.txt
+chmod a+rwx permissions.txt
+export TESTENV1="test"
+grep -E '\b\w{3,}\b' text.txt > regex.txt
+cd ..
+
+
